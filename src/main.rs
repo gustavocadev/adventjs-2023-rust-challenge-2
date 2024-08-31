95c0c885dd361fa24f73a31fe8f1d67abab89998
gustavocadev <gustavocadev@gmail.com>
2024-08-31 17:08:08 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn manufacture(gifs: Vec<&str>, materials: String) -> Vec<&str> {
+  let mut possible_gifs: Vec<&str> = vec![];
+
+  for gif in gifs {
+    let can_created_gif = gif.chars().all(|gif_char| {
+      let exists_material = materials.chars().any(|material| gif_char == material);
+      if exists_material {
+        return exists_material;
+      }
+      exists_material
+    });
+
+    if can_created_gif {
+      possible_gifs.push(gif);
+    }
+  }
+
+  return possible_gifs;
+}
+
+fn main() {
+  let gifs: Vec<&str> = vec!["tren", "oso", "pelota"];
+  let materials = String::from("tronesa");
+
+  println!("{:?}", manufacture(gifs, materials));
+}
